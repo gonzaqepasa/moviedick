6e784551e253da86e8dfc689fec4b0cdb1356764
Gonzalo Martinez <gonzalovam@hotmail.com>
2023-11-08 17:15:35 -0300

    Config for ViewText

--- src/components/MovieView/MovieView.tsx
+++ src/components/MovieView/MovieView.tsx
@@ -1,19 +1,19 @@
 import ViewImage from "./ViewImage";
 import TextView from "./TextView";
 
-
 interface Props {
   MovieMain: [];
+  textConfig?: {};
 }
 
-const MovieView: React.FC<Props> = ({ MovieMain }) => {
+const MovieView: React.FC<Props> = ({ MovieMain, textConfig }) => {
   // Logica de aleatoridad
   const random_number = Math.floor(Math.random() * MovieMain.length);
   const movie: any = MovieMain[random_number];
   return (
     <div className="relative  w-screen   md:h-110 ">
       <ViewImage movieMain={movie.backdrop_path} />
-      <TextView MovieMain={movie} />
+      <TextView textConfig={textConfig} MovieMain={movie} />
     </div>
   );
 };
