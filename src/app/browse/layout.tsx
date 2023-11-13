99d7d0ccb354ceab18497ce8da5749a5ab538e8e
Gonzalo Martinez <gonzalovam@hotmail.com>
2023-11-13 20:51:08 -0300

    Delete old nav

--- src/app/browse/layout.tsx
+++ src/app/browse/layout.tsx
@@ -1,11 +1,9 @@
 import Footer from "@/components/Footer/Footer";
-import Navbar from "@/components/Navbar/Navbar";
 import NewNav from "@/components/Navbar/NewNav";
 
 function LayoutBorse({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      {/* <Navbar /> */}
       <NewNav />
       {children}
       <Footer />
